efccf9e9ddc6b397452a49b9a596f633fb7b6225
jack young <jackjordanyoung@gmail.com>
2023-02-18 16:23:35 +0000

    refactor pattern nodes, added gitignore

--- regexTree.py
+++ regexTree.py
@@ -10,191 +10,17 @@ class NodeType(Enum):
     QUANT = 1
     PATTERN = 2
 
-# MATCH DATA JSON
-# {
-#     startPos: int,
-#     endPos: int,
-#     regex: string,
-#     string: string,
-#     previousData: [
-#         {
-#             another match data
-#         }
-#     ]
-# }
+class PatternType(Enum):
+    UNKOWN = -1
+    LITERAL = 0
+    RANGE = 1
+    CATEGORY = 2
 
-# DEFAULT_ANCHOR = {
-#     'startPos': 0,
-#     'endPos':
-# }
+class CategoryType(Enum):
+    UNKOWN = -1
+    DIGIT = 0,
+    WORD = 0,
 
-class regexNode:
-    def getName(self):
-        return ''
-
-    def getMatches(self, input, pastMatchData = None):
-        return pastMatchData
-
-class listNode(regexNode):
-    def __init__(self):
-        self.children = []
-    
-    def appendChild(self, node):
-        self.children.append(node)
-
-    def getName(self):
-        name = '('
-        for subData in self.children:
-            name += subData.getName()
-        return name + ')'
-
-    def getMatches(self, input, pastMatchData = None):
-        lastMatchData = pastMatchData
-        for child in self.children:
-            matchData = child.getMatches(input, lastMatchData)
-            if matchData:
-                lastMatchData = matchData
-            else:
-                return None
-        return lastMatchData
-
-class quantNode(regexNode):
-    # Range, a-b, i = sre_parse.MAXREPEAT
-
-    def __init__(self, lower, upper, childNode):
-        self.lower = lower
-        self.upper = upper
-        self.childNode = childNode
-
-    def getName(self):
-        if self.lower == 0 and self.upper == sre_parse.MAXREPEAT:
-            return f'{self.childNode.getName()}*'
-        elif self.lower == 1 and self.upper == sre_parse.MAXREPEAT:
-            return f'{self.childNode.getName()}+'
-        elif self.lower == 0 and self.upper == 1:
-            return f'{self.childNode.getName()}?'
-        elif self.upper == sre_parse.MAXREPEAT:
-            return f'{self.childNode.getName()}{{{self.lower},}}'
-        else:
-            return f'{self.childNode.getName()}{{{self.lower},{self.upper}}}'
-
-    def getMatches(self, input, pastMatchData = None):
-        outputMatchData = []
-        # Get matches form child
-        matchData = self.childNode.getMatches(input, pastMatchData)
-        if matchData:
-            for i in range(len(matchData)):
-                currentData = matchData[i]
-                # Get size of match
-                matchSize = currentData["endPos"] - currentData["startPos"] + 1
-                if matchSize >= self.lower and ((self.upper == sre_parse.MAXREPEAT) or (matchSize <= self.upper)):
-                    # Valid match size
-                    outputMatchData.append(currentData)
-            return None if len(outputMatchData) == 0 else outputMatchData
-        else:
-            return None
-
-class patternNode(regexNode):
-    def __init__(self, char):
-        self.char = char
-
-    def getName(self):
-        return self.char
-
-    def getMatches(self, input, pastMatchData = None):
-        if pastMatchData == None:
-            # If theres no anchor, start from 0
-            # TODO: if starting from 0 or anywhere match, LOOK INTO STARTING CHAR ALSO
-            matchData = self.getMatchesFromIndex(input, 0)
-
-            return matchData
-        else:
-            outputMatchData = []
-
-            # Starting from each anchor
-            for anchor in pastMatchData:
-                matchData = self.getMatchesFromIndex(input, anchor['endPos'] + 1)
-                # If failed to match
-                if matchData:
-                    for currentData in matchData:
-
-                        # Add previous match data and append
-                        currentData['previousData'] = anchor['previousData']
-                        # if len(currentData['previousData']) > 0:
-                        #     currentData['previousData'] = anchor['previousData']
-                        # currentData['previousData'].append(anchor)
-
-                        outputMatchData.append(currentData)
-                    return outputMatchData
-                else:
-                    return None
-
-    def getMatchesFromIndex(self, input, startIndex):
-        output = []
-
-        # Get match blocks
-        index = startIndex
-        matchRanges = []
-
-        print(index)
-        print(len(input))
-        print(self.char)
-        print(input[index])
-        print(True if index < len(input) else False)
-        print(True if re.match(self.char, input[index]) else False)
-        print(True if index < len(input) and re.match(self.char, input[index]) else False)
-
-        while index < len(input) and re.match(self.char, input[index]):
-            index += 1
-        
-        if index == startIndex:
-            return None
-        else:
-            ranges = getOrderedPerms(startIndex, index - startIndex)
-            for orderedTuple in ranges:
-                chars = ''
-                for c in orderedTuple:
-                    chars += str(input[c])
-                # output.append((orderedTuple[0], orderedTuple[-1], chars))
-                output.append({
-                    'startPos': orderedTuple[0],
-                    'endPos': orderedTuple[-1],
-                    'regex': self.char,
-                    'string': chars,
-                    'previousData': None
-                })
-            return output
-
-
-
-
-
-        # for offsetI in range(len(input) - index):
-        #     i = offsetI + index
-        #     if re.match(self.char, input[i]):
-        #         if startPos == -1:
-        #             startPos = i
-        #     elif startPos != -1:
-        #         matchRanges.append((startPos, i))
-        #         startPos = -1
-
-        # # Get all matches from blocks
-        # for matchRange in matchRanges:
-        #     ranges = getOrderedPerms(matchRange[0], matchRange[1] - matchRange[0])
-        #     for orderedTuple in ranges:
-        #         chars = ''
-        #         for c in orderedTuple:
-        #             chars += str(input[c])
-        #         output.append((orderedTuple[0], orderedTuple[-1], chars))
-        #         output.append({
-        #             'startPos': index,
-        #             'endPos': ,
-        #             'regex': self.char,
-        #             'string': 'idkyet',
-        #             'previousData': []
-        #         })
-
-        # return 
 
 def regexToNode(rawData): 
 
@@ -232,26 +58,42 @@ def regexToNode(rawData):
 
         elif rawData[0] == sre_parse.LITERAL:
             node['type'] = NodeType.PATTERN
-            node['value'] = chr(rawData[1]) + ""
+            node['value'] = {
+                'type' : PatternType.LITERAL,
+                'value' : chr(rawData[1]),
+                'regex' : chr(rawData[1]) + ""
+            }
 
         elif rawData[0] == sre_parse.IN:
             node['type'] = NodeType.PATTERN
-            value = ''
-
-            for charData in rawData[1]:
-                if charData[0] == sre_parse.CATEGORY:
-                    if charData[1] == sre_parse.CATEGORY_DIGIT:
-                        value += r'\d'
-                    elif charData[1] == sre_parse.CATEGORY_WORD:
-                        value += r'\w'
-                elif charData[0] == sre_parse.RANGE:
-                    value += f'[{chr(charData[1][0])}-{chr(charData[1][1])}]'
-                else:
-                    node['type'] = NodeType.UNKOWN
-                    node['value'] = None
-                    return node
-            
-            node['value'] = value
+            charData = rawData[1][0]
+            if len(rawData[1]) > 1:
+                print('ERROR CHECK THIS IN CODE')
+            if charData[0] == sre_parse.CATEGORY:
+                regex = ''
+                value = CategoryType.UNKOWN
+                if charData[1] == sre_parse.CATEGORY_DIGIT:
+                    regex = r'\d'
+                    value = CategoryType.DIGIT
+                elif charData[1] == sre_parse.CATEGORY_WORD:
+                    regex = r'\w'
+                    value = CategoryType.WORD
+                value = CategoryType.DIGIT
+                node['value'] = {
+                        'type' : PatternType.CATEGORY,
+                        'value' : value,
+                        'regex' : regex
+                    }
+            elif charData[0] == sre_parse.RANGE:
+                node['value'] = {
+                    'type' : PatternType.RANGE,
+                    'value' : (chr(charData[1][0]), chr(charData[1][1])),
+                    'regex' : f'[{chr(charData[1][0])}-{chr(charData[1][1])}]'
+                }
+            else:
+                node['type'] = NodeType.UNKOWN
+                node['value'] = None
+                return node
 
         else:
             node['type'] = NodeType.UNKOWN
@@ -280,7 +122,7 @@ def nodeToRegex(parentNode):
         else:
             return nodeToRegex(parentNode['value']['child']) + '{' + str(parentNode['value']['lower']) + ',' + str(parentNode['value']['upper']) + '}'
     elif parentNode['type'] == NodeType.PATTERN:
-        return parentNode['value']
+        return parentNode['value']['regex']
     return ''
 
 def getMatchData(parentNode, string, pastPos = 0):
@@ -356,7 +198,7 @@ def getMatchData(parentNode, string, pastPos = 0):
 
     elif parentNode['type'] == NodeType.PATTERN:
         output = []
-        matches = getUnitMatches(parentNode['value'], string, pastPos)
+        matches = getUnitMatches(parentNode['value']['regex'], string, pastPos)
         if matches:
             for i in matches:
                 i.update({'node' : parentNode})
